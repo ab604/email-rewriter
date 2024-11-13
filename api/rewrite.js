532eff5882c4386fabb69e351a73e475c7ef759f
ab604 <ab604@soton.ac.uk>
2024-11-13 18:17:07 +0000

    update tone select

--- api/rewrite.js
+++ api/rewrite.js
@@ -34,7 +34,7 @@ export default {
                 'passive aggressive': 'passive aggressive',
                 'hard boiled detective': 'hard boiled detective',
                 'melodramatic': '19th century melodramatic character',
-                'business' : 'Ricky Gervais from the Office', 
+                'business': 'Ricky Gervais from the Office', 
                 'meow like a cat': 'Meow',
                 'pompous': 'Pompous',
                 'Mr. Mistoffelees': 'Mr. Mistoffelees', 
